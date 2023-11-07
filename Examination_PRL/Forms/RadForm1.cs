69738ba510a83eb6b1f5e6b3270713beae164a72
SNAKE 07 <nguyendoanquangluc1112004@gmail.com>
2023-11-07 15:04:28 +0700

    giao diện thêm sinh viên

---
+++ Examination_PRL/Forms/RadForm1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Examination_PRL.Forms
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
