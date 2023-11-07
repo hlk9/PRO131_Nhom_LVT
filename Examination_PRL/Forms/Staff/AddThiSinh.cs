c832228a5f7369bc35c99f3ad5092a4fbb15f8e2
SNAKE 07 <nguyendoanquangluc1112004@gmail.com>
2023-11-07 15:06:47 +0700

    AdThiSinh

---
+++ Examination_PRL/Forms/Staff/AddThiSinh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Examination_PRL.Forms
+{
+    public partial class AddThiSinh : Telerik.WinControls.UI.RadForm
+    {
+        public AddThiSinh()
+        {
+            InitializeComponent();
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
