3b04355f80eefd94b2552b1194a5fb09de718b07
SNAKE 07 <nguyendoanquangluc1112004@gmail.com>
2023-11-09 12:30:14 +0700

    lai nho

---
+++ Examination_PRL/Forms/Staff/AddThiSinh.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Examination_PRL.Forms
+{
+    partial class AddThiSinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            materialTealTheme1 = new Telerik.WinControls.Themes.MaterialTealTheme();
+            radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            radMaskedEditBox1 = new Telerik.WinControls.UI.RadMaskedEditBox();
+            radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            radLabel4 = new Telerik.WinControls.UI.RadLabel();
+            radMaskedEditBox5 = new Telerik.WinControls.UI.RadMaskedEditBox();
+            radMaskedEditBox6 = new Telerik.WinControls.UI.RadMaskedEditBox();
+            radMaskedEditBox7 = new Telerik.WinControls.UI.RadMaskedEditBox();
+            radMaskedEditBox8 = new Telerik.WinControls.UI.RadMaskedEditBox();
+            radLabel5 = new Telerik.WinControls.UI.RadLabel();
+            radLabel6 = new Telerik.WinControls.UI.RadLabel();
+            radLabel7 = new Telerik.WinControls.UI.RadLabel();
+            radLabel8 = new Telerik.WinControls.UI.RadLabel();
+            radButton1 = new Telerik.WinControls.UI.RadButton();
+            radRadioButton1 = new Telerik.WinControls.UI.RadRadioButton();
+            radRadioButton2 = new Telerik.WinControls.UI.RadRadioButton();
+            radTextBoxControl1 = new Telerik.WinControls.UI.RadTextBoxControl();
+            radDateTimePicker1 = new Telerik.WinControls.UI.RadDateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)radLabel1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radButton1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radRadioButton1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radRadioButton2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radTextBoxControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radDateTimePicker1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)this).BeginInit();
+            SuspendLayout();
+            // 
+            // radLabel1
+            // 
+            radLabel1.Location = new Point(32, 44);
+            radLabel1.Name = "radLabel1";
+            radLabel1.Size = new Size(78, 21);
+            radLabel1.TabIndex = 2;
+            radLabel1.Text = "First Name";
+            radLabel1.ThemeName = "MaterialTeal";
+            // 
+            // radMaskedEditBox1
+            // 
+            radMaskedEditBox1.Location = new Point(344, 71);
+            radMaskedEditBox1.Name = "radMaskedEditBox1";
+            radMaskedEditBox1.Size = new Size(272, 42);
+            radMaskedEditBox1.TabIndex = 7;
+            radMaskedEditBox1.TabStop = false;
+            radMaskedEditBox1.ThemeName = "MaterialTeal";
+            // 
+            // radLabel2
+            // 
+            radLabel2.Location = new Point(344, 44);
+            radLabel2.Name = "radLabel2";
+            radLabel2.Size = new Size(77, 21);
+            radLabel2.TabIndex = 8;
+            radLabel2.Text = "Last Name";
+            radLabel2.ThemeName = "MaterialTeal";
+            // 
+            // radLabel3
+            // 
+            radLabel3.Location = new Point(32, 112);
+            radLabel3.Name = "radLabel3";
+            radLabel3.Size = new Size(72, 21);
+            radLabel3.TabIndex = 9;
+            radLabel3.Text = "Ngày Sinh";
+            radLabel3.ThemeName = "MaterialTeal";
+            // 
+            // radLabel4
+            // 
+            radLabel4.Location = new Point(344, 112);
+            radLabel4.Name = "radLabel4";
+            radLabel4.Size = new Size(65, 21);
+            radLabel4.TabIndex = 10;
+            radLabel4.Text = "Giới Tính";
+            radLabel4.ThemeName = "MaterialTeal";
+            // 
+            // radMaskedEditBox5
+            // 
+            radMaskedEditBox5.Location = new Point(32, 211);
+            radMaskedEditBox5.MaskType = Telerik.WinControls.UI.MaskType.EMail;
+            radMaskedEditBox5.Name = "radMaskedEditBox5";
+            radMaskedEditBox5.Size = new Size(272, 42);
+            radMaskedEditBox5.TabIndex = 13;
+            radMaskedEditBox5.TabStop = false;
+            radMaskedEditBox5.ThemeName = "MaterialTeal";
+            // 
+            // radMaskedEditBox6
+            // 
+            radMaskedEditBox6.Location = new Point(344, 211);
+            radMaskedEditBox6.Name = "radMaskedEditBox6";
+            radMaskedEditBox6.Size = new Size(272, 42);
+            radMaskedEditBox6.TabIndex = 14;
+            radMaskedEditBox6.TabStop = false;
+            radMaskedEditBox6.ThemeName = "MaterialTeal";
+            // 
+            // radMaskedEditBox7
+            // 
+            radMaskedEditBox7.Location = new Point(32, 287);
+            radMaskedEditBox7.Name = "radMaskedEditBox7";
+            radMaskedEditBox7.Size = new Size(272, 42);
+            radMaskedEditBox7.TabIndex = 15;
+            radMaskedEditBox7.TabStop = false;
+            radMaskedEditBox7.ThemeName = "MaterialTeal";
+            // 
+            // radMaskedEditBox8
+            // 
+            radMaskedEditBox8.Location = new Point(344, 287);
+            radMaskedEditBox8.Name = "radMaskedEditBox8";
+            radMaskedEditBox8.Size = new Size(272, 42);
+            radMaskedEditBox8.TabIndex = 16;
+            radMaskedEditBox8.TabStop = false;
+            radMaskedEditBox8.ThemeName = "MaterialTeal";
+            // 
+            // radLabel5
+            // 
+            radLabel5.Location = new Point(32, 184);
+            radLabel5.Name = "radLabel5";
+            radLabel5.Size = new Size(43, 21);
+            radLabel5.TabIndex = 17;
+            radLabel5.Text = "Email";
+            radLabel5.ThemeName = "MaterialTeal";
+            // 
+            // radLabel6
+            // 
+            radLabel6.Location = new Point(344, 184);
+            radLabel6.Name = "radLabel6";
+            radLabel6.Size = new Size(34, 21);
+            radLabel6.TabIndex = 18;
+            radLabel6.Text = "SĐT";
+            radLabel6.ThemeName = "MaterialTeal";
+            // 
+            // radLabel7
+            // 
+            radLabel7.Location = new Point(32, 260);
+            radLabel7.Name = "radLabel7";
+            radLabel7.Size = new Size(31, 21);
+            radLabel7.TabIndex = 19;
+            radLabel7.Text = "Lớp";
+            radLabel7.ThemeName = "MaterialTeal";
+            // 
+            // radLabel8
+            // 
+            radLabel8.Location = new Point(344, 260);
+            radLabel8.Name = "radLabel8";
+            radLabel8.Size = new Size(53, 21);
+            radLabel8.TabIndex = 20;
+            radLabel8.Text = "Địa Chỉ";
+            radLabel8.ThemeName = "MaterialTeal";
+            // 
+            // radButton1
+            // 
+            radButton1.BackColor = Color.LightSeaGreen;
+            radButton1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            radButton1.Location = new Point(32, 343);
+            radButton1.Name = "radButton1";
+            radButton1.Size = new Size(584, 51);
+            radButton1.TabIndex = 21;
+            radButton1.Text = "Thêm Thí Sinh";
+            radButton1.ThemeName = "MaterialTeal";
+            // 
+            // radRadioButton1
+            // 
+            radRadioButton1.Location = new Point(364, 142);
+            radRadioButton1.Name = "radRadioButton1";
+            radRadioButton1.Size = new Size(57, 22);
+            radRadioButton1.TabIndex = 22;
+            radRadioButton1.Text = "Nam";
+            radRadioButton1.ThemeName = "MaterialTeal";
+            // 
+            // radRadioButton2
+            // 
+            radRadioButton2.Location = new Point(477, 141);
+            radRadioButton2.Name = "radRadioButton2";
+            radRadioButton2.Size = new Size(45, 22);
+            radRadioButton2.TabIndex = 23;
+            radRadioButton2.Text = "Nữ";
+            radRadioButton2.ThemeName = "MaterialTeal";
+            // 
+            // radTextBoxControl1
+            // 
+            radTextBoxControl1.Location = new Point(35, 71);
+            radTextBoxControl1.Name = "radTextBoxControl1";
+            radTextBoxControl1.Size = new Size(269, 36);
+            radTextBoxControl1.TabIndex = 25;
+            radTextBoxControl1.ThemeName = "MaterialTeal";
+            // 
+            // radDateTimePicker1
+            // 
+            radDateTimePicker1.CalendarSize = new Size(290, 320);
+            radDateTimePicker1.Location = new Point(32, 139);
+            radDateTimePicker1.Name = "radDateTimePicker1";
+            radDateTimePicker1.Size = new Size(280, 41);
+            radDateTimePicker1.TabIndex = 26;
+            radDateTimePicker1.TabStop = false;
+            radDateTimePicker1.Text = "Tuesday, November 7, 2023";
+            radDateTimePicker1.ThemeName = "MaterialTeal";
+            radDateTimePicker1.Value = new DateTime(2023, 11, 7, 14, 57, 47, 519);
+            // 
+            // RadForm1
+            // 
+            AutoScaleBaseSize = new Size(8, 20);
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(650, 429);
+            Controls.Add(radDateTimePicker1);
+            Controls.Add(radTextBoxControl1);
+            Controls.Add(radRadioButton2);
+            Controls.Add(radRadioButton1);
+            Controls.Add(radButton1);
+            Controls.Add(radLabel8);
+            Controls.Add(radLabel7);
+            Controls.Add(radLabel6);
+            Controls.Add(radLabel5);
+            Controls.Add(radMaskedEditBox8);
+            Controls.Add(radMaskedEditBox7);
+            Controls.Add(radMaskedEditBox6);
+            Controls.Add(radMaskedEditBox5);
+            Controls.Add(radLabel4);
+            Controls.Add(radLabel3);
+            Controls.Add(radLabel2);
+            Controls.Add(radMaskedEditBox1);
+            Controls.Add(radLabel1);
+            Name = "RadForm1";
+            // 
+            // 
+            // 
+            RootElement.ApplyShapeToControl = true;
+            Text = "RadForm1";
+            ThemeName = "MaterialTeal";
+            Load += RadForm1_Load;
+            ((System.ComponentModel.ISupportInitialize)radLabel1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radMaskedEditBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radLabel8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radButton1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radRadioButton1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radRadioButton2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radTextBoxControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radDateTimePicker1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)this).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Telerik.WinControls.Themes.MaterialTealTheme materialTealTheme1;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadMaskedEditBox radMaskedEditBox1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel radLabel3;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
+        private Telerik.WinControls.UI.RadMaskedEditBox radMaskedEditBox5;
+        private Telerik.WinControls.UI.RadMaskedEditBox radMaskedEditBox6;
+        private Telerik.WinControls.UI.RadMaskedEditBox radMaskedEditBox7;
+        private Telerik.WinControls.UI.RadMaskedEditBox radMaskedEditBox8;
+        private Telerik.WinControls.UI.RadLabel radLabel5;
+        private Telerik.WinControls.UI.RadLabel radLabel6;
+        private Telerik.WinControls.UI.RadLabel radLabel7;
+        private Telerik.WinControls.UI.RadLabel radLabel8;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton1;
+        private Telerik.WinControls.UI.RadRadioButton radRadioButton2;
+        private Telerik.WinControls.UI.RadTextBoxControl radTextBoxControl1;
+        private Telerik.WinControls.UI.RadDateTimePicker radDateTimePicker1;
+    }
+}
