860e66d5d4e5dc127c03477453b56f3d0307967e
duc2004288@gmail.com <duc2004288@gmail.com>
2023-11-18 21:44:44 +0700

    phongthi

--- Examination_PRL/Forms/Staff/Room.cs
+++ Examination_PRL/Forms/Staff/Room.cs
@@ -21,6 +21,7 @@ namespace Examination_PRL.Forms.Staff
         {
             InitializeComponent();
             _ser = new ClassroomServices();
+            LoadData(_ser.GetClassrooms());
         }
 
         public void LoadData(dynamic data)
@@ -38,67 +39,8 @@ namespace Examination_PRL.Forms.Staff
         }
 
 
-        private void btn_AddRoom_Click(object sender, EventArgs e)
-        {
-            string name = tb_Room.Text;
-            string ID = tb_RoomID.Text;
-            bool add = _ser.AddNewClass(name, ID);
-            if (add) MessageBox.Show("Thêm thành công!");
-            else MessageBox.Show("Thêm thất bại!");
-            LoadData(_ser.GetClassrooms());
-        }
 
-        private void Room_Load(object sender, EventArgs e)
-        {
-            LoadData(_ser.GetClassrooms());
-        }
 
-        private void dtg_Show_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int rowIndex = e.RowIndex;
-            var selectedClass = dtg_Show.Rows[rowIndex];
-            tb_Room.Text = selectedClass.Cells[1].Value.ToString();
-            tb_RoomID.Text = selectedClass.Cells[2].Value.ToString();
-        }
 
-        private void btn_DeleteRoom_Click(object sender, EventArgs e)
-        {
-
-            var DialogResult = MessageBox.Show("Bạn có muốn xóa không?", "Xác nhận", MessageBoxButtons.YesNo);
-            if (DialogResult == DialogResult.Yes)
-            {
-                Classroom room = new Classroom();
-                string name = tb_Room.Text;
-                string ID = tb_RoomID.Text;
-                _idWhenClick = Convert.ToInt32(room.Id);
-                bool delete = _ser.RemoveClass(ID);
-                if (delete) MessageBox.Show("Xóa thành công!");
-                else MessageBox.Show("Xóa thất bại!");
-                LoadData(_ser.GetClassrooms());
-
-            }
-            else
-            {
-                return;
-            }
-        }
-
-        private void btn_Clear_Click(object sender, EventArgs e)
-        {
-            tb_Room.Text = "";
-            tb_RoomID.Text = "";
-        }
-
-        private void btn_EditRoom_Click(object sender, EventArgs e)
-        {
-            Classroom room = new Classroom();
-            string name = tb_Room.Text;
-            string ID = tb_RoomID.Text;
-
-            bool update = _ser.UpdateClass(ID, name);
-            if (update) MessageBox.Show("Sửa thành công!");
-            else MessageBox.Show("Sửa thất bại!");
-            LoadData(_ser.GetClassrooms());
-        }
     }
 }
