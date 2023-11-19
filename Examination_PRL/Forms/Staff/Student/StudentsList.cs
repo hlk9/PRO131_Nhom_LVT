4c65c89f57bba8186e06e0f0eb7da5b5ed7c2dd0
Bac <bac5120048a4@gmail.com>
2023-11-19 14:34:38 +0700

    fix

---
+++ Examination_PRL/Forms/Staff/Student/StudentsList.cs
@@ -0,0 +1,49 @@
+﻿using Examination_BUS.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Examination_PRL.Forms.Staff
+{
+    public partial class StudentsList : Telerik.WinControls.UI.RadForm
+    {
+        ParticipantService _ser = new ParticipantService();
+
+        public StudentsList()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            examGridView.ColumnCount = 7;
+            examGridView.Columns[0].HeaderText = "Mã Thí Sinh";
+            examGridView.Columns[1].HeaderText = "Tên Thí Sinh";
+            examGridView.Columns[2].HeaderText = "Giới Tính";
+            examGridView.Columns[3].HeaderText = "Ngày Sinh";
+            examGridView.Columns[4].HeaderText = "Số Điện Thoại";
+            examGridView.Columns[5].HeaderText = "Email";
+            examGridView.Columns[6].HeaderText = "Địa Chỉ";
+
+            foreach (var x in _ser.getAllStudents())
+            {
+                examGridView.Rows.Add(x.Id, x.FullName, (x.Gender == true ? "Nam" : "Nữ"), x.DateOfBirth, x.PhoneNumber, x.Email, x.Address);
+            }
+        }
+
+        private void examGridView_UserDeletingRow(object sender, Telerik.WinControls.UI.GridViewRowCancelEventArgs e)
+        {
+            string id=  examGridView.CurrentRow.Cells[0].Value.ToString();
+            MessageBox.Show("Ping"+id);
+            //get id current cell
+            
+
+        }
+    }
+}
---
+++ Examination_PRL/Forms/Staff/Student/UpdateStudents.cs
@@ -0,0 +1,71 @@
+﻿using Examination_BUS.Services;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Examination_PRL.Forms.Staff
+{
+    public partial class UpdateStudents : Telerik.WinControls.UI.RadForm
+    {
+        ParticipantService _ser = new ParticipantService();
+
+        string _idWhenClick;
+
+        public UpdateStudents()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            int stt = 1;
+
+            examGridView.ColumnCount = 8;
+            examGridView.Columns[0].HeaderText = "STT";
+            examGridView.Columns[1].HeaderText = "Mã Thí Sinh";
+            examGridView.Columns[2].HeaderText = "Tên Thí Sinh";
+            examGridView.Columns[3].HeaderText = "Giới Tính";
+            examGridView.Columns[4].HeaderText = "Ngày Sinh";
+            examGridView.Columns[5].HeaderText = "Số Điện Thoại";
+            examGridView.Columns[6].HeaderText = "Email";
+            examGridView.Columns[7].HeaderText = "Địa Chỉ";
+
+            examGridView.Rows.Clear();
+
+            foreach (var x in _ser.getAllStudents())
+            {
+                examGridView.Rows.Add(stt++, x.Id, x.FullName, (x.Gender == true ? "Nam" : "Nữ"), x.DateOfBirth, x.PhoneNumber, x.Email, x.Address);
+            }
+        }
+
+        private void examGridView_CellClick(object sender, Telerik.WinControls.UI.GridViewCellEventArgs e)
+        {
+            _idWhenClick = examGridView.Rows[e.RowIndex].Cells[1].Value.ToString();        
+            var obj = _ser.getAllStudents().Where(x => x.Id == _idWhenClick).FirstOrDefault();
+
+            if (obj != null)
+            {
+                radTxtTen.Text = obj.FullName;
+                radTxtPhone.Text = obj.PhoneNumber;
+                radTxtEmail.Text = obj.Email;
+                radTxtAdress.Text = obj.Address;
+                radDtpNS.Value = obj.DateOfBirth;
+                if(obj.Gender == true)
+                {
+                    radBNam.IsChecked = true;
+                }
+                else
+                {
+                    radBNu.IsChecked = true;
+                }
+            }
+        }
+    }
+}
