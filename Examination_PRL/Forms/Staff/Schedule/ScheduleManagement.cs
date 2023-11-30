67bcccf9f3f409493c3b791ab6823fb57882bdb3
Hoang Long <hoanglong.cmb@gmail.com>
2023-11-30 14:49:03 +0700

    add schedule form

---
+++ Examination_PRL/Forms/Staff/Schedule/ScheduleManagement.cs
@@ -0,0 +1,26 @@
+﻿using Examination_BUS.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Examination_PRL.Forms.Staff.Schedule
+{
+    public partial class ScheduleManagement : Telerik.WinControls.UI.RadForm
+    {
+        ScheduleServices scheduleServices = new ScheduleServices();
+        public ScheduleManagement()
+        {
+            InitializeComponent();
+        }
+        public void LoadData()
+        {
+
+
+        }
+    }
+}
