f4bebcdb5bcc98301dc639203bafb439749db67c
Hoang Long <hoanglong.cmb@gmail.com>
2023-12-05 13:17:11 +0700

    ssss

--- Examination_PRL/Properties/Resources.Designer.cs
+++ Examination_PRL/Properties/Resources.Designer.cs
@@ -123,16 +123,6 @@ namespace Examination_PRL.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap logoForTestify {
-            get {
-                object obj = ResourceManager.GetObject("logoForTestify", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         public static System.Drawing.Bitmap person {
             get {
                 object obj = ResourceManager.GetObject("person", resourceCulture);
