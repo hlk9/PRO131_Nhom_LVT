8e0042a95e7e08c624bc9c531ba833b452140604
duc2004288@gmail.com <duc2004288@gmail.com>
2024-03-28 00:36:50 +0700

    duc

---
+++ Examination.Test/Unit/AnswerService_Test.cs
@@ -0,0 +1,34 @@
+﻿using Examination_BUS.Services;
+using Examination_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Test.Unit
+{
+    public class AnswerService_Test
+    {
+        Examination_BUS.Services.AnswerServices _service;
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new Examination_BUS.Services.AnswerServices();
+        }
+
+        [Test]
+        public void GetAllAns()
+        {
+            Assert.IsNotNull(_service.GetAllAnswers());
+        }
+
+        [Test]
+
+        public void AddAns_Pass()
+        {
+            Answer ans = new Answer { Id = 1, Content = "First Answer", IsCorrect = true , CreatedAt = DateTime.Now , UpdatedAt = DateTime.Now , CreatedBy="admin" ,UpdatedBy="admin" };
+            Assert.AreEqual(true, _service.AddAnswer(ans));
+        }
+    }
+}
---
+++ Examination.Test/Unit/Subject_Service_Test.cs
@@ -0,0 +1,36 @@
+﻿using Examination_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Test.Unit
+{
+    public class Subject_Service_Test
+    {
+        Examination_BUS.Services.SubjectServices _service;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            _service = new Examination_BUS.Services.SubjectServices();
+
+        }
+
+        [Test]
+        public void GetAllSubjetc()
+        {
+            Assert.IsNotNull(_service.GetSubjects());
+        }
+
+        [Test]
+
+        public void AddSubjetc_Pass()
+        {
+            Subject sbj = new Subject { Id = "T104", Name="Toán",Status = 1, Description = "Mô tả 1"};
+            //Assert.AreEqual(true, _service.AddSub(sbj));
+        }
+    }
+}
