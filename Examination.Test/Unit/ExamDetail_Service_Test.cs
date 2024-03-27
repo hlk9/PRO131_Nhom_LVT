e154120ea437b674fdf5ae4a74383287a3595c03
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-27 21:54:02 +0700

    first commit

--- Examination.Test/Unit/ExamDetail_Service_Test.cs
+++ Examination.Test/Unit/ExamDetail_Service_Test.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Examination_DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,13 @@ namespace Examination.Test.Unit
         public void GetAllExamDetail()
         {
             Assert.IsNotNull(_service.GetAll());
+        }
+
+        [Test]
+        public void AddExam_Pass()
+        {
+            ExamDetail ex = new ExamDetail { ExamDetailCode = "ED002", ExamId = 2, Duration = 90, PassMark = 60.0, TotalQuestion = 30, MaxiumMark = 150.0, CreatedAt = DateTime.Now, CreatedBy = "admin" };
+            Assert.AreEqual(true, _service.Add(ex));
         }
     }
 }
