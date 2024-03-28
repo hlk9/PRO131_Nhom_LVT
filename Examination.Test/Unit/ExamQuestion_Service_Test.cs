2dcbc9b50393bdc96d55fc18c1a907032608ea48
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-28 14:57:46 +0700

    1 st

---
+++ Examination.Test/Unit/ExamQuestion_Service_Test.cs
@@ -0,0 +1,47 @@
+﻿using Examination_BUS.Services;
+using Examination_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Test.Unit
+{
+    public class ExamQuestion_Service_Test
+    {
+        Examination_BUS.Services.ExamQuestionServices _service;
+        [SetUp]
+        public void Setup()
+        {
+            _service = new Examination_BUS.Services.ExamQuestionServices();
+        }
+
+        [Test]
+        public void GetAllExamQuestion()
+        {
+            Assert.IsNotNull(_service.GetAllExamQuestions());
+        }
+
+        [Test]
+        public void AddExam_Pass()
+        {
+            ExamQuestion question = new ExamQuestion();
+
+            //question.Id = " admin ";
+            //question.Content = " Kiem Tra Mot Tiet";
+            //question.Point = " 10";
+            //question.QuestionType = " Trắc Nghiệm";
+            //question.QuestionLevel = "Trung Bình";
+            //question.Subject = "Toán";
+            //question.Docs = "Hình Học Không gian";
+            //question.CreatedTime = "19/03/2024"
+            //question.ModifiedTime = "20/03/2024";
+            //question.CreatedBy = "GV CXH";
+            //question.ModifiedBy = "GV CXH";
+            //question.Status = "1";
+
+            //Assert.AreEqual(true, _service.AddExamQuestion());
+        }
+    }
+}
---
+++ Examination.Test/Unit/ExamResponse_Service_Test.cs
@@ -0,0 +1,49 @@
+﻿using Examination_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Test.Unit
+{
+    public class ExamResponse_Service_Test
+    {
+        Examination_BUS.Services.ExamResponseServices _service;
+        [SetUp]
+        public void Setup()
+        {
+            _service = new Examination_BUS.Services.ExamResponseServices();
+        }
+
+        [Test]
+        public void GetAllExamQuestion()
+        {
+            Assert.IsNotNull(_service.GetAllExamResponse());
+        }
+
+        [Test]
+        public void AddExam_Pass()
+        {
+            ExamResponse exRes = new ExamResponse();
+
+            exRes.Id = 1;
+            exRes.ExamDetailId = 1;
+            exRes.ParticipantId = "longhd";
+            exRes.SubjectId = "MAth01";
+            exRes.SubmitTime = DateTime.Parse("19/03/2024");
+            exRes.Score = 10;
+            exRes.Status = 1;
+            exRes.QuestionCorrect = 10;
+            exRes.QuestionWrong = 0;
+            exRes.QuestionNotAnswered = 0;
+            exRes.ScoredBy = "";
+            exRes.FinishTime = 10;
+            exRes.ScoredMethod =true;
+            exRes.Note = "Không";
+            exRes.ExamScheduleId = 1;
+
+            //Assert.AreEqual(true, _service.AddExamQuestion());
+        }
+    }
+}
