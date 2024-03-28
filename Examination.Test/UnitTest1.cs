c94421cacb335c78f481dd5e56ff55384cdefe17
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-28 15:00:46 +0700

    add test proj

---
+++ Examination.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Examination.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
