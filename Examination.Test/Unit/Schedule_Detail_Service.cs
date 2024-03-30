98aae76a416d29befd88fbf4fc2299dfb7769043
Hopcx <admin@LAPTOP-CXH2410>
2024-03-30 07:41:35 +0700

    30/3

---
+++ Examination.Test/Unit/Schedule_Detail_Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Test.Unit
+{
+    public  class Schedule_Detail_Service
+    {
+
+    }
+}
---
+++ Examination.Test/Unit/User_Permission_Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Test.Unit
+{
+    public class User_Permission_Service
+    {
+    }
+}
